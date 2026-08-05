ead0405eddfbd8c41b84f879b105ddc412fe2b90
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-05 16:36:56 +0530

    use comparison on the same datatypes and different datatypes

---
+++ 01_basics/comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 == 2);
+// console.log(2 > 3);
+// console.log(2 >= 2);
+// console.log(2 < 4);
+// console.log(2 <= 4);
+// console.log(2 != 4);
+
+// console.log("2" == 2); //true because this dont check the datatype
+// console.log("02" == 2);
+// console.log("2" > 2);
+// console.log("2" >= 2);
+
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null > 0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined > 0);
+
+// Strict Check also checks the datatype (===)-->
+console.log("2" === 2); //gives false because the data type is differ

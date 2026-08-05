bb3764521f85126afeda5c873c384e6576e9a191
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-05 15:19:58 +0530

    explore differ datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,13 @@
+"use Strict" //treat java code as new version
+
+let num = 12345;
+let name = "Nitish";
+let IsLogin = true;
+let Price;
+let Reward = null;
+
+console.log(typeof Price); //this gives undefined
+console.log(typeof Reward); //this gives the type object
+console.log(typeof num);
+console.log(typeof name);
+console.log(typeof IsLogin);

8a360ca029f5989da0df6c0c4574d8bb7b6de7a3
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-05 16:17:04 +0530

    use oprations on variables

---
+++ 01_basics/oprations.js
@@ -0,0 +1,33 @@
+//Airthmetic Oprations-->
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let value = 18;
+let NegValue = -value;
+
+console.log(NegValue);
+
+let str1 = "Hello ";
+let str2 = "World!!!";
+
+console.log(str1 + str2);
+
+console.log(1 + "2"); //12
+console.log("1" + 2); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+console.table([num1,num2,num3]);
+
+let count = 100;
+// count++;
+// ++count;
+// count--;
+// --count;
+console.log(count);

80f90cf72c3c6565b5c296150535cf522a9e865b
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-05 15:48:47 +0530

    learn type conversuion of datatypes

---
+++ 01_basics/conersionOprations.js
@@ -0,0 +1,29 @@
+let score = "1";
+
+//console.log(typeof(score));
+
+let NumberTo = Number(score);
+//console.log(NumberTo);
+//console.log(typeof(NumberTo));
+
+// "100abc" --> NaN (Not a Number)
+// "Hitesh" --> Number
+// "1" --> Number
+
+let IsLogin = 0;
+//console.log(typeof(IsLogin));
+
+let InBool = Boolean(IsLogin);
+//console.log(InBool);
+//console.log(typeof InBool);
+
+// 1 --> true, 0 --> false
+// "" => false
+// "hitesh" => true
+
+let Name = 123;
+console.log(typeof(Name));
+
+let InString = String(Name);
+console.log(InString);
+console.log(typeof(InString));

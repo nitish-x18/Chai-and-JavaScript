17e79f5a64fcc463028808be6f4210f17689b972
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-07 00:40:40 +0530

    explore more Array functions

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -32,3 +32,30 @@ console.log(OldArr);
 
 console.log(OldArr.splice(1,4)); //remove the elemenets includes the range
 console.log(OldArr);
+
+let Marvel_Heros = ["IronMan", "SpiderMan", "Thor"];
+let Dc_Heros = ["SuperMan", "BatMan", "FlyingJat"];
+
+//Marvel_Heros.push(Dc_Heros);
+
+// console.log(Marvel_Heros);
+// console.log(Marvel_Heros[3][0]);
+
+let All_Heros = Marvel_Heros.concat(Dc_Heros);
+//console.log(All_Heros);
+
+//console.log([...Marvel_Heros, ...Dc_Heros]); //Spred
+
+console.log(Array.isArray("Nitish"));
+console.log(Array.from("Nitish")) //convert into Array
+console.log(Array.from({Name: "Nitish"})) //cant give the Array because its not classified that key or pair of array want user so its return empty array
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
+
+let Complex = [1,2,3,4,[2,3,4],3,4,3,[9,8,[6,3,5]]];
+let Simple = Complex.flat(Infinity);
+console.log(Simple);

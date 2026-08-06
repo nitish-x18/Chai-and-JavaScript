80d3447102a7c5d27b8e20caade18ef4a3a5fadf
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-06 17:37:08 +0530

    explore math inbuild functios

--- 01_basics/Number_&_maths.js
+++ 01_basics/Number_&_maths.js
@@ -11,3 +11,24 @@ console.log(Balance.toLocaleString('en-IN')); //by default its convert into usa 
 let Num = 123.8262
 console.log(Num.toPrecision(4))
 console.log(Num.toFixed(2))
+
+// ***************************** MATHS ********************************
+
+console.log(Math);
+
+console.log(Math.abs(-2));
+console.log(Math.round(4.32213));
+console.log(Math.ceil(4.32213));
+console.log(Math.floor(4.92213));
+console.log(Math.pow(2,3));
+console.log(Math.max(2,5,7,8,9,5,3));
+console.log(Math.min(2,5,7,8,9,5,3));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.ceil((Math.random() * 10) + 1));
+
+let max = 920;
+let min = 910;
+
+console.log(Math.ceil(Math.random() * (max - min + 1) + min));

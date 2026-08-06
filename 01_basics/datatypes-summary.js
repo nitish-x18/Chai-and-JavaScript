1a82b5751e65606039fddf64b9e1ca2cc5d34a6b
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-06 16:42:37 +0530

    explore about the numbers in JS

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,67 @@
+// Premitive Data Types-->
+// 7 --> Number, String, Bigint, Boolean, Null, Undefined, Symbol
+
+const Score = 100; //Number
+let Name = "Nitish" //String
+let BigNumber = 1889788713442n //Bigint
+let Isloggedin = true //Boolean
+let Empty; //Undefined
+let Temprature = null //Object
+
+console.log(typeof(Score));
+console.log(typeof(Name));
+console.log(typeof(BigNumber));
+console.log(typeof(Isloggedin));
+console.log(typeof(Empty));
+console.log(typeof(Temprature));
+
+let Sym = Symbol('123')
+let Sym1 = Symbol('123')
+
+console.log(Sym === Sym1);
+
+console.log(typeof(Sym));
+
+// Non-Premitive Data Types-->
+// array, object, Function
+
+let Hero = ["IronMan", "SpiderMan", "Thor"]; //Object
+
+let MyObj = {
+    Name: "Peter Parker",
+    Id: 18
+} //Object
+
+function myfunc(){
+    console.log("Hello World!!!");
+} //function
+
+myfunc(); //call function 
+
+console.log(typeof(Hero));
+console.log(typeof(MyObj));
+console.log(typeof(myfunc));
+
+//********************** Stack & Heap ************************************/
+
+//in the stack memory the copy of data is passed and all are premitive datatypes are used
+let Myname = "Nitish";
+
+let anotherName = Myname;
+anotherName = "Yadav";
+
+console.log(Myname);
+console.log(anotherName);
+
+
+//in heap meamory the refrence of data is passed and non-premitive datatypes is used
+let UserOne = {
+    Email: "nitish@gmail.com",
+    Num: 7070126532
+}
+
+let SecondUser = UserOne;
+
+SecondUser.Email = "Kuchbhi@gmail.com";
+
+console.log(UserOne);

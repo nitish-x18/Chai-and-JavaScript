a3eec410225b397b87f220594260946ac7aafcbc
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-08 12:49:19 +0530

    learn about scope level and mini hoisting

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -12,3 +12,38 @@ if (true) {
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function One(){
+    const UserName = "Nitish";
+
+    function Two(){
+        const Website = "Youtube";
+        console.log(UserName);
+    }
+    
+    //console.log(Website); //give error becouse out of scope
+    Two();
+}
+
+One();
+
+if(true){
+    const UserName = "Nitish";
+    if(UserName === "Nitish"){
+        const Website = " Google";
+        console.log(UserName + Website);
+    }
+}
+
+console.log(AddOne(5));
+
+function AddOne(num){
+    return num + 1;
+}
+
+const AddTwo = function(num){
+    return num + 2;
+}
+
+console.log(AddTwo(5));

7dabe5c201b9113d282ed4e6aea0d8134f337b67
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-08 12:00:38 +0530

    handle object and array in functions and learn about rest

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -21,3 +21,39 @@ function UserDetail(UserName){
 }
 
 // UserDetail();
+
+
+function CalculateCartPrice(val1, val2, ...items){ //(...) rest function that takes all the arguments
+    return items;
+}
+
+// console.log(CalculateCartPrice(20,39,405,30,28));
+// console.log(CalculateCartPrice(10,20,30,40,50));
+
+let Obj = {
+    UserName: "Nitish",
+    price: 10000
+}
+
+
+//Pass Object In function-->
+function MyObj(anyobject){
+    console.log(`${anyobject.UserName} is User and price is ${anyobject.price}`);
+}
+
+// MyObj(Obj);
+// MyObj(UserDetail = {
+//     UserName: "Hitesh",
+//     price: 1
+// })
+
+//Pass Array in function-->
+
+let Arr = [10,20,30,40,50];
+
+function SecondValueofArr(anyarray){
+    console.log(anyarray[1]);
+}
+
+// SecondValueofArr(Arr);
+// SecondValueofArr([90,80,70,60,50]);

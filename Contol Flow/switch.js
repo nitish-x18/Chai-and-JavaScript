ce32fe217d3c33a221e202630064bc9d8bef3f8b
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-09 23:52:21 +0530

    practice switch case

---
+++ Contol Flow/switch.js
@@ -0,0 +1,30 @@
+// Switch Case-->
+
+const Day = 7;
+switch (Day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thrusday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+
+    default:
+        console.log("Enter number Between 1-7");
+        break;
+}

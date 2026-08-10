76fb1b5b0a37ddd4160f418a35109014056bfe36
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-10 21:35:48 +0530

    use maps in js

---
+++ 05_Iteration/map.js
@@ -0,0 +1,13 @@
+let MyNums = [1,2,3,4,5,6,7,8,9,10];
+
+let NewNums = MyNums.map( (num) => {
+    // return num;
+})
+
+// let NewNums = MyNums.map( (num) => num * 10);
+
+let NewNums = MyNums.map( (num) => num * 10)
+                .map( (num) => num + 1)
+                .filter((num) => num >= 50);
+
+console.log(NewNums);

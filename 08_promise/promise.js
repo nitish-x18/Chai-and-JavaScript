607cf01c337a3fc0cf0d832546a7782efcf28649
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-15 01:46:37 +0530

    learn about the promises in js

---
+++ 08_promise/promise.js
@@ -0,0 +1,52 @@
+const PromiseOne = new Promise( function(resolve, reject) {
+    //Do async task 
+    //DB tasks, Cryptography, Network tasks
+
+    setTimeout( () => {
+        console.log('Async task is complete');
+        resolve();
+    },1000)
+})
+
+PromiseOne.then( () => {
+    console.log('Promise Consumed');
+})
+
+new Promise( (resolve, reject) => {
+    setTimeout(() => {
+        console.log('Async Task 2 completed');
+        resolve();
+    }, 2000);
+}).then( () => {
+    console.log('Async task 2 resolved');
+})
+
+const PromiseThree = new Promise( (resolve, reject) => {
+    setTimeout( () => {
+        resolve({userName: "Nitish", email: "Chai@gmail.com"})
+    }, 1000)
+})
+
+PromiseThree.then( (user) => {
+    console.log(user);
+})
+
+const PromiseFour = new Promise( (resolve, reject) => {
+    let error = false;
+    if(!error){
+        resolve({userName: "Nitish", Pass: 123});
+    } else {
+        reject('ERROR: Something Went Wrong');
+    }
+})
+
+PromiseFour.then( (user) => {
+    console.log(user);
+    return user.userName;
+}).then( (userName) => {
+    console.log(userName);
+}).catch( (error) => {
+    console.log(error)
+}).finally( () => {
+    console.log('Resolve or Reject Something can happen');
+})

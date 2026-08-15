5d65c178df272818f96d9ae66b5d97eea755d6aa
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-15 15:08:28 +0530

    learn basic of oops

---
+++ 09_classes/oops.js
@@ -0,0 +1,31 @@
+const User = {
+    UserName: "Nitish",
+    Email: "Nitish@gmail.chai",
+    IsLoggedIn: true,
+
+    Getfunc: function(){
+        console.log('Hello World');
+        console.log(`Welcome, ${this.UserName}`);
+    }
+}
+
+// console.log(User.UserName);
+// console.log(User.Getfunc());
+
+const Users = function(UserName, Email, IsLoggedIn){
+    this.UserName = UserName;
+    this.Email = Email;
+    this.IsLoggedIn = IsLoggedIn;
+
+    this.greet = (() => {
+        console.log(`Hello, ${this.UserName}`);
+    })
+
+    return this;
+}
+
+let userOne = new Users('Hitesh', 'abc@gmail.com', true);
+let userTwo = new Users('IronMan', 'iron@gmail.com', false);
+console.log(userOne);
+console.log(userOne.greet());
+console.log(userTwo);

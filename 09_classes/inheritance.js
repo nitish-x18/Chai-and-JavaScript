867197953f08478bd574f17ea3fcdaa7a24a9443
nitish yadav <yadavmunnakumar951@gmail.com>
2026-08-15 19:36:33 +0530

    familiar with inheritance in js

---
+++ 09_classes/inheritance.js
@@ -0,0 +1,26 @@
+// Old Syntax-->
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// Modern Syntax-->
+
+Object.setPrototypeOf(TeachingSupport, Teacher)
